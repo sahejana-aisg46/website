759ecf07888030d81186843ec826dc450cf72bc3
unknown <ynarula678@gmail.com>
2021-10-01 11:42:06 +0530

    slides team member initialized

---
+++ app.js
@@ -0,0 +1,13 @@
+ var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        }
+});
