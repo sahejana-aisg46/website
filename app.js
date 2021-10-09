def7d47ebf85509dac72112cc7f2602289609a19
unknown <ynarula678@gmail.com>
2021-10-09 22:32:54 +0530

    lightbox video gallery

--- app.js
+++ app.js
@@ -98,4 +98,29 @@ s4.addEventListener('click',()=>{
     });
   })(jQuery);
   
-  $('.overlay').addClass('overlay-blue');
+  // $('.overlay').addClass('overlay-blue');
+  // fetch(`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${process.env.REACT_APP_INSTAGRAM_ACCESS_TOKEN}`).then((res)=> {
+  //           const response = res.json().then((data)=> {
+  //               const token = data.access_token 
+  //               fetch(`https://graph.instagram.com/me/media?fields=id&access_token=${token}`).then((idsres)=> {
+  //                   idsres.json().then((idsjson)=> {
+  //                       const ids = idsjson.data 
+  //                       for (var i =0; i < 5 ; i++) {
+  //                           fetch(`https://graph.instagram.com/${ids[i].id}?fields=media_url,permalink,media_type&access_token=${token}`).then((media)=> {
+  //                               media.json().then((mediaData)=> {
+  //                                   if(mediaData.media_type!="VIDEO") {
+  //                                       const media = {
+  //                                           link: mediaData.permalink,
+  //                                           image: mediaData.media_url,
+  //                                       }
+  //                                       setInsta(insta.concat(media))
+  //                                   }
+  //                               })
+  //                           })
+  //                       } 
+  //                       console.log(insta)
+                        
+  //                   })
+  //               }) 
+  //           })
+  //       })
